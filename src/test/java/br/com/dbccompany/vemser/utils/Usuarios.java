7248d588ad708b12b43568def6f46b910abc9366
Gabriel Kleiman <kleim4n@gmail.com>
2023-04-09 11:29:26 -0300

    fix/alterado nomenclatura de variaveis de ambiente

--- src/test/java/br/com/dbccompany/vemser/utils/Usuarios.java
+++ src/test/java/br/com/dbccompany/vemser/utils/Usuarios.java
@@ -2,8 +2,11 @@ package br.com.dbccompany.vemser.utils;
 
 public class Usuarios {
     //Variavel de ambiente DBC_USER = usuario,senha,nome
-    public static final String[] USUARIO_DADOS = System.getenv("DBC_USER").split(",");
-    public static final String USUARIO = USUARIO_DADOS[0];
-    public static final String SENHA = USUARIO_DADOS[1];
-    public static final String NOME = USUARIO_DADOS[2];
+//    public static final String[] USUARIO_DADOS = System.getenv("DBC_USER").split(",");
+
+//    public static final String USUARIO = USUARIO_DADOS[0];
+//    public static final String SENHA = USUARIO_DADOS[1];
+//    public static final String NOME = USUARIO_DADOS[2];
+    public static final String USUARIO = System.getenv("DBC_USER");
+    public static final String SENHA = System.getenv("DBC_PASSWORD");
 }
