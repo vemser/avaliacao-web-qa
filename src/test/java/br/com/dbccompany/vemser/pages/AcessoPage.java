8c638bae3b4c6f38768bd604bea927d031e761c0
Gabriel Kleiman <kleim4n@gmail.com>
2023-04-12 20:50:37 -0300

    feat/cenario de teste de logout

--- src/test/java/br/com/dbccompany/vemser/pages/AcessoPage.java
+++ src/test/java/br/com/dbccompany/vemser/pages/AcessoPage.java
@@ -1,10 +1,22 @@
 package br.com.dbccompany.vemser.pages;
 
 import br.com.dbccompany.vemser.utils.Elementos;
+import org.openqa.selenium.By;
 
 public class AcessoPage extends Elementos {
-    public static final String SELETOR_NOME_USUARIO = "div.usuario.MuiBox-root.css-0 > div :nth-child(1)";
-    public static final String SELETOR_USUARIO = "div.usuario.MuiBox-root.css-0 > div :nth-child(2)";
+    public static final String SELETOR_BOTAO_SAIR = "[data-testid=\"LogoutIcon\"]";
+    public static final String SELETOR_ICONE_USUARIO = "[data-testid=\"PersonIcon\"]";
+    public static final String SELETOR_NOME_USUARIO = "div.nome-usuario > h3";
+    public static final String SELETOR_USUARIO = "div.nome-usuario > span";
+    public void clicarBotaoAvaliacao() {
+        driver.findElement(By.linkText("Avaliação")).click();
+    }
+    public void clicarBotaoSair() {
+        clicar(SELETOR_BOTAO_SAIR);
+    }
+    public void clicarIconeUsuario() {
+        clicar(SELETOR_ICONE_USUARIO);
+    }
     public String consultarNomeUsuarioLogado() {
         esperarConteudoNaoVazio(SELETOR_NOME_USUARIO);
         return consultarTexto(SELETOR_NOME_USUARIO).toLowerCase();
@@ -12,5 +24,9 @@ public class AcessoPage extends Elementos {
     public String consultarUsuarioLogado() {
         esperarConteudoNaoVazio(SELETOR_USUARIO);
         return consultarTexto(SELETOR_USUARIO).toLowerCase();
+    }
+    public void realizarLogoutComSucesso() {
+        clicarIconeUsuario();
+        clicarBotaoSair();
     }
 }
--- src/test/java/br/com/dbccompany/vemser/steps/LoginStepTest.java
+++ src/test/java/br/com/dbccompany/vemser/steps/LoginStepTest.java
@@ -5,6 +5,7 @@ import br.com.dbccompany.vemser.pages.LoginPage;
 import br.com.dbccompany.vemser.utils.Navegador;
 import io.qameta.allure.Description;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,11 @@ import org.junit.jupiter.api.Test;
 public class LoginStepTest extends Navegador {
     AcessoPage acessoPage = new AcessoPage();
     LoginPage loginPage = new LoginPage();
+    @AfterEach
+    public void limparCookies(){
+        driver.manage().deleteAllCookies();
+        loginPage.acessarPagina();
+    }
     @Test
     @Story("Usuário faz login")
     @Description("Usuário faz login com sucesso")
@@ -24,11 +30,13 @@ public class LoginStepTest extends Navegador {
         Assertions.assertEquals(loginPage.consultarUsuarioValido(), acessoPage.consultarUsuarioLogado());
         Assertions.assertEquals(loginPage.consultarNomeUsuarioValido(), acessoPage.consultarNomeUsuarioLogado());
     }
-//    @Test
-//    @Story("Usuário faz login")
-//    @Description("Usuário faz logout de sua conta com sucesso")
-//    public void testLogout(){
-//        loginPage.realizarLoginComSucesso();
-//
-//    }
+    @Test
+    @Story("Usuário faz login")
+    @Description("Usuário faz logout de sua conta com sucesso")
+    public void testLogout(){
+        loginPage.realizarLoginComSucesso();
+        acessoPage.clicarIconeUsuario();
+        acessoPage.clicarBotaoSair();
+        Assertions.assertTrue(loginPage.estaNaPaginaDeLogin());
+    }
 }
