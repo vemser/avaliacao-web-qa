03cfba55040fce59c7bf5cd8687477cc2762d1c9
Gabriel Kleiman <kleim4n@gmail.com>
2023-04-30 18:34:41 -0300

    fix/adicionado delays ao teste de demonstracao

--- src/test/java/br/com/dbccompany/vemser/steps/DemonstracaoStepTest.java
+++ src/test/java/br/com/dbccompany/vemser/steps/DemonstracaoStepTest.java
@@ -15,10 +15,19 @@ public class DemonstracaoStepTest extends Navegador {
     private static LoginPage loginPage = new LoginPage();
     private static AcessoPage acessoPage = new AcessoPage();
     private static MenuPage menuPage = new MenuPage();
-    private static ConfiguracoesPage configuracoesPage = new ConfiguracoesPage();
+    private ConfiguracoesPage configuracoesPage = new ConfiguracoesPage();
     public static EstagiariosPage estagiariosPage = new EstagiariosPage();
     private static EstagiariosCadastroPage estagiariosCadastroPage = new EstagiariosCadastroPage();
     private static EstagiariosInformacoesPage estagiariosInformacoesPage = new EstagiariosInformacoesPage();
+    public void timer(int milisegundos) {
+        configuracoesPage.esperarTempo(milisegundos);
+    }
+    public void timerCurto() {
+        timer(500);
+    }
+    public void timerMedio() {
+        timer(1000);
+    }
     @Test
     @Disabled
     @DisplayName("Demonstração do sistema")
@@ -27,15 +36,25 @@ public class DemonstracaoStepTest extends Navegador {
         acessoPage.estaNaPaginaAcesso();
         // navegar pelo menu
         menuPage.acessarPagina();
+        timerCurto();
         menuPage.clicarBotaoDashboard();
+        timerMedio();
         menuPage.clicarBotaoEstagiarios();
+        timerMedio();
         menuPage.clicarBotaoComportamental();
+        timerMedio();
         menuPage.clicarBotaoAcompanhamentos();
+        timerMedio();
         menuPage.clicarBotaoTecnico();
+        timerMedio();
         menuPage.clicarBotaoTecnicoFeedback();
+        timerMedio();
         menuPage.clicarBotaoConfiguracoes();
+        timerMedio();
         // cadastrar estagiário
-        estagiariosPage.acessarPagina();
+        menuPage.clicarBotaoEstagiarios();
+        timerMedio();
+        timerMedio();
         estagiariosPage.clicarBotaoCadastroEstagiario();
         EstagiarioModel estagiario = EstagiarioDataFactory.gerarEstagiarioValido(1);
         estagiariosCadastroPage.preencherCampoNome(estagiario.getNome());
@@ -52,35 +71,49 @@ public class DemonstracaoStepTest extends Navegador {
         estagiariosCadastroPage.preencherCampoLinkedin(estagiario.getLinkedin());
         estagiariosCadastroPage.preencherCampoObservacoes(estagiario.getObservacoes());
         estagiariosCadastroPage.clicarSelecionarPrograma();
+        timerCurto();
         estagiariosCadastroPage.selecionarOpcaoPrograma(0);
         estagiariosCadastroPage.clicarSelecionarTrilha();
+        timerCurto();
         estagiariosCadastroPage.selecionarOpcaoTrilha(0);
         estagiariosCadastroPage.clicarSelecionarStatus();
+        timerCurto();
         estagiariosCadastroPage.selecionarOpcaoStatus(0);
         estagiariosCadastroPage.clicarBotaoCadastrar();
         estagiariosPage.fecharModal();
+        timerMedio();
         // consultar por nome
         estagiariosPage.filtrarEstagiarioPorNome(estagiario.getNome());
         estagiariosPage.clicarBotaoBuscar();
         estagiariosPage.esperarBuscaPorNome(estagiario.getNome());
+        timerMedio();
         estagiariosPage.clicarBotaoDetalhesDoEstagiarioPorIdValido();
+        timerMedio();
         // alterar nome do estagiário
         estagiariosPage.clicarBotaoEditarEstagiario();
         estagiario.setNome(estagiario.getNome() + " Editado");
         estagiariosCadastroPage.esperarBuscaDeCandidatoValido();
         estagiariosCadastroPage.limparCampoNome();
         estagiariosCadastroPage.preencherCampoNome(" Editado");
+        timerMedio();
         estagiariosCadastroPage.clicarBotaoEditar();
         estagiariosPage.fecharModal();
         // consultar por nome
+        timerCurto();
         estagiariosPage.filtrarEstagiarioPorNome(estagiario.getNome());
+        timerMedio();
         estagiariosPage.clicarBotaoBuscar();
+        timerMedio();
         estagiariosPage.esperarBuscaPorNome(estagiario.getNome());
+        timerMedio();
         estagiariosPage.clicarBotaoDetalhesDoEstagiarioPorIdValido();
         // desativar estagiário
+        timerMedio();
         estagiariosInformacoesPage.clicarBotaoDesativarEstagiario();
+        timerMedio();
         estagiariosPage.preencherCampoMotivoDesativacaoValido();
         estagiariosPage.clicarBotaoConfirmarDesativarEstagiario();
+        timerMedio();
     }
     @AfterAll
     public static void limparMassa() {
