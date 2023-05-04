2c1fd646a5142b73dd92bcc56cb353f62f15ac24
Gabriel Kleiman <kleim4n@gmail.com>
2023-05-04 15:12:00 -0300

    feat/fluxo de avaliação: cadastro de massa

---
+++ src/test/java/br/com/dbccompany/vemser/steps/AvaliacaoStepTest.java
@@ -0,0 +1,69 @@
+package br.com.dbccompany.vemser.steps;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import br.com.dbccompany.vemser.pages.AcessoPage;
+import br.com.dbccompany.vemser.pages.ConfiguracoesPage;
+import br.com.dbccompany.vemser.pages.EstagiariosCadastroPage;
+import br.com.dbccompany.vemser.pages.EstagiariosInformacoesPage;
+import br.com.dbccompany.vemser.pages.EstagiariosPage;
+import br.com.dbccompany.vemser.pages.LoginPage;
+import br.com.dbccompany.vemser.pages.MenuPage;
+import br.com.dbccompany.vemser.utils.Navegador;
+import dataFactory.EstagiarioDataFactory;
+import model.EstagiarioModel;
+import service.EstagiarioService;
+
+public class AvaliacaoStepTest extends Navegador {
+    private static LoginPage loginPage = new LoginPage();
+    private static AcessoPage acessoPage = new AcessoPage();
+    private static MenuPage menuPage = new MenuPage();
+    private ConfiguracoesPage configuracoesPage = new ConfiguracoesPage();
+    public static EstagiariosPage estagiariosPage = new EstagiariosPage();
+    private static EstagiariosCadastroPage estagiariosCadastroPage = new EstagiariosCadastroPage();
+    private static EstagiariosInformacoesPage estagiariosInformacoesPage = new EstagiariosInformacoesPage();
+    private static String nomeEstagiario;
+    @BeforeAll
+    public static void loginECriarMassa() {
+        loginPage.realizarLoginComSucesso();
+        acessoPage.estaNaPaginaAcesso();
+        estagiariosPage.acessarPagina();
+        estagiariosPage.clicarBotaoCadastroEstagiario();
+        EstagiarioModel estagiario = EstagiarioDataFactory.gerarEstagiarioValido(1);
+        estagiariosCadastroPage.preencherCampoNome(estagiario.getNome());
+        estagiariosCadastroPage.preencherCampoCpf(estagiario.getCpf());
+        estagiariosCadastroPage.preencherCampoEmailPessoal(estagiario.getEmailPessoal());
+        estagiariosCadastroPage.preencherCampoEmailCorporativo(estagiario.getEmailCorporativo());
+        estagiariosCadastroPage.preencherCampoTelefone(estagiario.getTelefone());
+        estagiariosCadastroPage.preencherCampoDataNascimento(estagiario.getDataNascimento());
+        estagiariosCadastroPage.preencherCampoEstado(estagiario.getEstado());
+        estagiariosCadastroPage.preencherCampoCidade(estagiario.getCidade());
+        estagiariosCadastroPage.preencherCampoInstituicaoEnsino(estagiario.getInstituicaoEnsino());
+        estagiariosCadastroPage.preencherCampoCurso(estagiario.getCurso());
+        estagiariosCadastroPage.preencherCampoGithub(estagiario.getGithub());
+        estagiariosCadastroPage.preencherCampoLinkedin(estagiario.getLinkedin());
+        estagiariosCadastroPage.preencherCampoObservacoes(estagiario.getObservacoes());
+        estagiariosCadastroPage.clicarSelecionarPrograma();
+        estagiariosCadastroPage.selecionarOpcaoPrograma(0);
+        estagiariosCadastroPage.clicarSelecionarTrilha();
+        estagiariosCadastroPage.selecionarOpcaoTrilha(0);
+        estagiariosCadastroPage.clicarSelecionarStatus();
+        estagiariosCadastroPage.selecionarOpcaoStatus(0);
+        estagiariosCadastroPage.clicarBotaoCadastrar();
+        Assertions.assertTrue(estagiariosPage.existeMensagemModal());
+        estagiariosPage.fecharModal();
+        nomeEstagiario = estagiario.getNome();
+    }
+    @AfterAll
+    public static void limparMassa() {
+        EstagiarioService.deletarEstagiariosTeste();
+    }
+    @Test
+    public void testFluxoDeAvaliacaoComSucesso() {
+        menuPage.clicarBotaoTecnico();
+        menuPage.clicarBotaoTecnicoFeedback();
+    }
+}
