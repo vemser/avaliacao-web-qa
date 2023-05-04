93a782890b33ab60cb48881458d1eecf0ba098c7
Gabriel Kleiman <kleim4n@gmail.com>
2023-05-04 15:37:06 -0300

    feat/funcao de clicar em detalhes do primeiro estagiario listado

--- src/test/java/br/com/dbccompany/vemser/pages/TecnicoFeedbackPage.java
+++ src/test/java/br/com/dbccompany/vemser/pages/TecnicoFeedbackPage.java
@@ -4,7 +4,15 @@ import br.com.dbccompany.vemser.utils.Elementos;
 
 public class TecnicoFeedbackPage extends Elementos {
     public static final String URL_PAGINA = URL_BASE + "/avaliacao-front/tecnico/feedback";
+    private static final String SELETOR_ACOMPANHAMENTOS = "[data-testid^='tecnico-card-menu-acompanhamento-']";
+    private static final String SELETOR_BOTOES_DETALHES = "[data-field='acoes'] [title='Botão Visualizar']";
     public void acessarPagina() {
         acessarUrl(URL_PAGINA);
+    }
+    public void clicarAcompanhamento() {
+        clicar(SELETOR_ACOMPANHAMENTOS);
+    }
+    public void clicarPrimeiroBotaoDetalhes() {
+        clicar(SELETOR_BOTOES_DETALHES);
     }
 }
