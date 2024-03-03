c7da5a27ec443610dce426905eb3b93b22c9eb97
gabriel johann <gabriel.johann102@gmail.com>
2024-03-03 17:27:25 -0300

    wip: corrigindo erro chromedriver

--- src/test/java/br/com/dbccompany/vemser/utils/Navegador.java
+++ src/test/java/br/com/dbccompany/vemser/utils/Navegador.java
@@ -15,9 +15,10 @@ public class Navegador {
     public static final String URL_BASE = "https://avaliacao-front-five.vercel.app";
     @BeforeEach
     public void abrirNavegador(){
-        //ChromeOptions semInterface = new ChromeOptions();
-        //semInterface.addArguments("--headless=new");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.navigate().to(URL_BASE + "/login");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
